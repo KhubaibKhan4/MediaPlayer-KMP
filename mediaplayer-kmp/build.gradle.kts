d7418796e470ad0261000c6b62339aafbb2c1567
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-07-06 12:24:02 +0500

    -Gpg Setup (#3)

--- mediaplayer-kmp/build.gradle.kts
+++ mediaplayer-kmp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     id("com.vanniktech.maven.publish") version "0.29.0"
+    id("com.google.osdetector") version "1.7.3"
 }
 
 kotlin {
@@ -52,11 +53,29 @@ kotlin {
         }
         val androidMain by getting {
             dependencies {
+                implementation(libs.startup.runtime)
+                implementation(libs.androidx.media3.exoplayer)
+                implementation(libs.androidx.media3.exoplayer.dash)
+                implementation(libs.androidx.media3.ui)
+                implementation(libs.custom.ui)
             }
         }
-
         val jvmMain by getting {
             dependencies {
+                val fxSuffix = when (osdetector.classifier) {
+                    "linux-x86_64" -> "linux"
+                    "linux-aarch_64" -> "linux-aarch64"
+                    "windows-x86_64" -> "win"
+                    "osx-x86_64" -> "mac"
+                    "osx-aarch_64" -> "mac-aarch64"
+                    else -> throw IllegalStateException("Unknown OS: ${osdetector.classifier}")
+                }
+                implementation("org.openjfx:javafx-base:19:${fxSuffix}")
+                implementation("org.openjfx:javafx-graphics:19:${fxSuffix}")
+                implementation("org.openjfx:javafx-controls:19:${fxSuffix}")
+                implementation("org.openjfx:javafx-swing:19:${fxSuffix}")
+                implementation("org.openjfx:javafx-web:19:${fxSuffix}")
+                implementation("org.openjfx:javafx-media:19:${fxSuffix}")
             }
         }
         val jsMain by getting {
