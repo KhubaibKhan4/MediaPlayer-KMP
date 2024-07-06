3f4e789b674dba2a062d275ee74b3ef9b0575576
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-07-06 12:55:25 +0500

    -Web Video Player Impl (#5)

--- mediaplayer-kmp/src/iosMain/kotlin/YouTubePlayer.ios.kt
+++ mediaplayer-kmp/src/iosMain/kotlin/YouTubePlayer.ios.kt
@@ -1,10 +1,62 @@
 import androidx.compose.runtime.Composable
+import platform.AVFoundation.AVPlayer
+import platform.AVFoundation.AVPlayerLayer
+import platform.AVFoundation.play
+import platform.AVKit.AVPlayerViewController
+import platform.CoreGraphics.CGRect
+import platform.Foundation.*
+import platform.Foundation.NSError
+import platform.Foundation.NSErrorDomain
+import platform.Foundation.NSHTTPURLResponse
+import platform.Foundation.NSURL
+import platform.Foundation.NSURLConnection
+import platform.Foundation.NSURLRequest
+import platform.Foundation.sendSynchronousRequest
+import platform.QuartzCore.CATransaction
+import platform.QuartzCore.kCATransactionDisableActions
+import platform.UIKit.*
+import platform.UIKit.UIActivityViewController
+import platform.UIKit.UIAlertAction
+import platform.UIKit.UIAlertController
+import platform.UIKit.UIApplication
+import platform.UIKit.UIDevice
+import platform.UIKit.UIView
+import platform.darwin.*
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
-actual fun VideoPlayer(
-    modifier: Modifier,
-    url: String?,
-    thumbnail: String?,
-) {
+actual fun VideoPlayer(modifier: Modifier, url: String?, thumbnail: String?) {
+
+    val player = remember { NSURL.URLWithString(url.toString())?.let { AVPlayer(uRL = it) } }
+    val playerLayer = remember { AVPlayerLayer() }
+    val avPlayerViewController = remember { AVPlayerViewController() }
+    avPlayerViewController.player = player
+    avPlayerViewController.showsPlaybackControls = true
+
+    playerLayer.player = player
+    UIKitView(
+        factory = {
+            val playerContainer = UIView()
+            playerContainer.addSubview(avPlayerViewController.view)
+            playerContainer
+        },
+        onResize = { view: UIView, rect: CValue<CGRect> ->
+            CATransaction.begin()
+            CATransaction.setValue(true, kCATransactionDisableActions)
+            view.layer.setFrame(rect)
+            playerLayer.setFrame(rect)
+            avPlayerViewController.view.layer.frame = rect
+            CATransaction.commit()
+        },
+        update = { view ->
+            player!!.play()
+            avPlayerViewController.player!!.play()
+        },
+        modifier = modifier
+    )
 }
--- mediaplayer-kmp/src/jsMain/kotlin/YouTubePlayer.js.kt
+++ mediaplayer-kmp/src/jsMain/kotlin/YouTubePlayer.js.kt
@@ -1,9 +1,58 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.browser.document
 
 @Composable
-actual fun VideoPlayer(
-    modifier: Modifier,
-    url: String?,
-    thumbnail: String?,
-) {
+actual fun VideoPlayer(modifier: Modifier, url: String?, thumbnail: String?) {
+    val videoId = extractVideoId(url.toString())
+    CompositionLocalProvider(LocalLayerContainer provides document.body!!) {
+        HTMLVideoPlayer(videoId)
+    }
+
+}
+
+@Composable
+fun HTMLVideoPlayer(videoId: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HtmlView(
+            modifier = Modifier.fillMaxWidth().height(300.dp),
+            factory = {
+                val iframe = createElement("iframe")
+                iframe.setAttribute("width", "100%")
+                iframe.setAttribute("height", "100%")
+                iframe.setAttribute(
+                    "src",
+                    "https://www.youtube.com/embed/$videoId?autoplay=1&mute=1&showinfo=0"
+                )
+                iframe.setAttribute("frameborder", "0")
+                iframe.setAttribute(
+                    "allow",
+                    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                )
+                iframe.setAttribute("allowfullscreen", "true")
+                iframe.setAttribute("referrerpolicy", "no-referrer-when-downgrade")
+                iframe
+            }
+        )
+    }
+}
+
+
+private fun extractVideoId(url: String): String {
+    val videoIdRegex =
+        Regex("""(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})""")
+    val matchResult = videoIdRegex.find(url)
+    return matchResult?.groupValues?.get(1) ?: "default_video_id"
 }
