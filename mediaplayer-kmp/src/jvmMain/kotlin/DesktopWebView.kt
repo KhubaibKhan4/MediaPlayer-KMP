5efd445a45e71eeb46405372ac2b1f9bed92ffa1
KhubaibKhan4 <18.bscs.803@gmail.com>
2024-12-17 21:49:20 +0500

    -Desktop Win App Crash Fixed #35

--- mediaplayer-kmp/src/jvmMain/kotlin/DesktopWebView.kt
+++ mediaplayer-kmp/src/jvmMain/kotlin/DesktopWebView.kt
@@ -1,36 +1,16 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.awt.SwingPanel
 import javafx.application.Platform
 import javafx.concurrent.Worker
 import javafx.embed.swing.JFXPanel
 import javafx.scene.Scene
 import javafx.scene.web.WebView
-import javax.swing.JPanel
 
-@Composable
-fun DesktopWebView(
-    modifier: Modifier,
-    url: String,
-    autoPlay: Boolean
-) {
-    val jPanel: JPanel = remember { JPanel() }
-    val jfxPanel = JFXPanel()
-
-    SwingPanel(
-        factory = {
-            jfxPanel.apply { buildWebView(url,autoPlay) }
-            jPanel.add(jfxPanel)
-        },
-        modifier = modifier,
-    )
-
-    DisposableEffect(url) { onDispose { jPanel.remove(jfxPanel) } }
+fun initJavaFX() {
+    System.setProperty("prism.order", "sw")
+    System.setProperty("prism.verbose", "true")
 }
 
-private fun JFXPanel.buildWebView(url: String,autoPlay: Boolean) {
+private fun JFXPanel.buildWebView(url: String, autoPlay: Boolean) {
+    initJavaFX()
     Platform.runLater {
         val webView = WebView()
         val webEngine = webView.engine
@@ -39,7 +19,6 @@ private fun JFXPanel.buildWebView(url: String,autoPlay: Boolean) {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
         webEngine.isJavaScriptEnabled = true
-
         webEngine.load(url)
 
         val scene = Scene(webView)
